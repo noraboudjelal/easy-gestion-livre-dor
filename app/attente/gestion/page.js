a6d4f4e4f2b09bb3fccd48dbc81b71c32ea26059
noraboudjelal <nora.boudjelal@outlook.com>
2026-09-06 17:27:04 +0200

    Ajouter les pages séparées Lehnova Attente

---
+++ app/attente/gestion/page.js
@@ -0,0 +1,4 @@
+import AttenteDashboard from "./AttenteDashboard";
+export const metadata = { title: "Gestion · Lehnova Attente" };
+export default function Page() { return <AttenteDashboard />; }
+

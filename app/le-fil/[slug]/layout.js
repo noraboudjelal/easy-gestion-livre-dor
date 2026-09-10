0690a9e7425328320d4a7202e222dc4ed3fa1ede
noraboudjelal <nora.boudjelal@outlook.com>
2026-09-10 16:57:40 +0200

    Show photo cover above Le Fil

--- app/le-fil/[slug]/layout.js
+++ app/le-fil/[slug]/layout.js
@@ -1,8 +1,10 @@
 import IdleCover from "./IdleCover";
+import FilPhotoCover from "./FilPhotoCover";
 
 export default function LeFilEventLayout({ children }) {
   return (
     <>
+      <FilPhotoCover />
       {children}
       <IdleCover timeoutMs={30000} />
     </>
